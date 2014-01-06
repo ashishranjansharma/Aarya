69071c208b3dde055921ee43d41445cf81670863
Ashish R Sharma <ashishrsharma@Ashishs-MacBook-Pro-2.local>
2014-01-06 21:34:08 +0530

    Initial Commit

---
+++ Learning2Code/Learning2Code/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Learning2Code
+//
+//  Created by Ashish R Sharma on 1/6/14.
+//  Copyright (c) 2014 Ashish R Sharma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
